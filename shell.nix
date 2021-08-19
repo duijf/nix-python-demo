5de098d6437a8f62a780b97ae0770953e4c134ab
Laurens Duijvesteijn <git@duijf.io>
2021-08-19 11:27:05 +0200

    Document `shell.nix`

--- shell.nix
+++ shell.nix
@@ -1,5 +1,11 @@
 let
+  # Import Nixpkgs so we get the package set and the "standard library".
+  # See the file `nix/nixpkgs.nix` for details and background.
   pkgs = import ./nix/nixpkgs.nix {};
+
+  # Define a Python environment. The exact versions aren't specified here, but
+  # they *are* pinned. Nixpkgs provides a snapshot of the ecosystem. You can
+  # override package versions to specific ones if you'd like though.
   pythonEnv = pkgs.python38.withPackages (ps: [
     ps.asyncpg
     ps.black
@@ -17,12 +23,14 @@ let
     ps.uvicorn
   ]);
 in
-pkgs.mkShell {
-  name = "nix-python-example";
-  buildInputs = [
-    pythonEnv
-    pkgs.overmind
-    pkgs.postgresql_13
-    pkgs.watchexec
-  ];
-}
+  # Create a development environment with our Python packages and a few system
+  # tools. Use Postgres 13 as our database. (This also makes the right version of
+  # `libpq` available.)
+  pkgs.mkShell {
+    name = "nix-python-example";
+    buildInputs = [
+      pythonEnv           # Python environment.
+      pkgs.overmind       # To manage our processes in the dev setup.
+      pkgs.postgresql_13  # Database.
+    ];
+  }
